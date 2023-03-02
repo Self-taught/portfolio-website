6541e187cd98f37711f5aa8682435379fa6effda
Self-taught <63093667+Self-taught@users.noreply.github.com>
2023-03-02 23:19:41 +0530

    Add and Edit functions in GUI

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
-def get_todos(filename):
+def get_todos(filename="todos.txt"):
     with open(filename, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath="todos.txt"):
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
--- gui.py
+++ gui.py
@@ -1,14 +1,38 @@
 import PySimpleGUI as sg
+import functions
 
-
+list_box = sg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[45,10])
+edit_button = sg.Button("Edit")
 layout = [[sg.Text("Enter a To-Do")],
-          [sg.InputText()],
-          [sg.Button('Add')] ]
+          [sg.InputText(key="todo")],
+          [sg.Button('Add')],
+          [list_box, edit_button]]
 
-window = sg.Window("A TO-DO App", layout)
+window = sg.Window("A TO-DO App", layout, font=("Helvetica", 20))
 
-event, values = window.read()
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
 
-print("hello", values[0], "Does this works?")
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values["todo"] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "Edit":
+            new_todo = values["todo"]
+            todo_to_edit = values["todos"][0]
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "todos":
+            window["todo"].update(value=values["todos"][0])
+        case sg.WIN_CLOSED:
+            break
 
 window.close()
