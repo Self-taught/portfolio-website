dec6a083935cef855e309ac4c3a304730dbec946
Self-taught <63093667+Self-taught@users.noreply.github.com>
2023-03-06 11:42:45 +0530

    Added data.csv and images. Created the first basic page of portfolio app using streamlit.

--- portfolio-website/main.py
+++ portfolio-website/main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import streamlit as st
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
 
+with col1:
+    st.image("images/cute me.png", width=380)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with col2:
+    st.title("Me on a regular day!")
+    content = """
+    Hi, Usually, I spend my days learning and coding python, javascript, and some other basic tech.
+    This is one of my first portfolio project that I am uploading on github.
+    """
+    st.info(content)
